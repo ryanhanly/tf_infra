4f5e715e4e1c5ef3762ace7caa85912ff697a4b3
ryanhanly <ryanhanly@me.com>
2025-04-14 11:34:02 +0100

    fix var errors

--- stack2-azure/outputs.tf
+++ stack2-azure/outputs.tf
@@ -1,11 +1,11 @@
 output "vm_public_ips" {
   description = "Public IPs of all VMs"
   value = {
-    for name, vm in module.ubuntu_vm : name => vm.public_ip
+    for name, vm in module.linux_vm : name => vm.public_ip
   }
 }
 
 output "resource_group_name" {
   description = "The name of the resource group"
-  value       = azurerm_resource_group.main.name
+  value       = azurerm_resource_group.name
 }
