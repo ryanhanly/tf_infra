e51d814f060e44fdabd6c7c9fc8c82516b808bd9
ryanhanly <ryanhanly@me.com>
2025-06-12 10:42:41 +0100

    from macbook

--- stack2-azure/main.tf
+++ stack2-azure/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "${var.infprefix}-${each.key}-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 # Create a network security group
